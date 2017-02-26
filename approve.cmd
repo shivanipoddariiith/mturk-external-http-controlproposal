d4e7c46f54f7be376eefd577b6b537bbc2eb8361
Xu Wang <xuwang@cs.cmu.edu>
2017-02-26 17:21:43 -0500

    upload

---
+++ approve.cmd
@@ -0,0 +1,22 @@
+@echo off
+rem
+rem Copyright 2008 Amazon Technologies, Inc.
+rem 
+rem Licensed under the Amazon Software License (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at:
+rem 
+rem http://aws.amazon.com/asl
+rem 
+rem This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+rem OR CONDITIONS OF ANY KIND, either express or implied. See the
+rem License for the specific language governing permissions and
+rem limitations under the License.
+rem 
+rem
+
+C:
+cd \mech-turk-tools-1.3.1\bin
+call approveWork -successfile E:\SocialLinguistics\metaphor\Mturk\metaphor_v1.input.success
+E:
+cd \SocialLinguistics\metaphor\Mturk
---
+++ uploadHITs.cmd
@@ -0,0 +1,23 @@
+@echo off
+rem
+rem Copyright 2008 Amazon Technologies, Inc.
+rem 
+rem Licensed under the Amazon Software License (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at:
+rem 
+rem http://aws.amazon.com/asl
+rem 
+rem This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+rem OR CONDITIONS OF ANY KIND, either express or implied. See the
+rem License for the specific language governing permissions and
+rem limitations under the License.
+rem 
+rem
+
+ 
+C:
+cd \mech-turk-tools-1.3.1\bin
+call loadHITs %1 %2 %3 %4 %5 %6 %7 %8 %9 -input E:\SocialLinguistics\Coursera\MturkSmallGroup-Xu\mturk-external-http-controlproposal\metaphor_v1.input -question E:\SocialLinguistics\Coursera\MturkSmallGroup-Xu\mturk-external-http-controlproposal\hit.question -properties E:\SocialLinguistics\Coursera\MturkSmallGroup-Xu\mturk-external-http-controlproposal\hit.properties 
+E:
+cd E:\SocialLinguistics\Coursera\MturkSmallGroup-Xu\mturk-external-http-controlproposal
